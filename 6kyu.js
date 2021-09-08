429e13edacfe0067ce83b3f9249f04f8af8feaee
Dmitriy Korolov <adrorecords@gmail.com>
2021-09-08 19:52:00 +0300

    add tasks

---
+++ 6kyu.js
@@ -0,0 +1,50 @@
+// ======== Array.diff ========
+// Your goal in this kata is to implement a difference function, 
+// which subtracts one list from another and returns the result.
+// It should remove all values from list a, which are present in list b keeping their order.
+// arrayDiff([1,2],[1]) == [2]
+// If a value is present in b, all of its occurrences must be removed from the other:
+// arrayDiff([1,2,2,2,3],[2]) == [1,3]
+
+// function arrayDiff(a, b) {
+//   let result = [];
+//   if (b.length === 0) {
+//     return a;
+//   }
+//   for (let i = 0; i < b.length; i++) {
+//     for (let x = 0; x < a.length; x++) {
+//       if (b[i] === a[x]) {
+//         a.splice(x, 1);
+//         x --;
+//       }
+//     }
+//   }
+//     return a;
+// }
+
+
+// ======== Sum of Digits / Digital Root ========
+// Digital root is the recursive sum of all the digits in a number.
+// Given n, take the sum of the digits of n.If that value has more than one digit, 
+// continue reducing in this way until a single - digit number is produced.The input will be a non - negative integer.
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+// function digital_root(n) {
+//   let result = 0;
+//   while (n > 9) {
+//   let arrayNumber = n.toString().split("");
+//   for (let digit of arrayNumber) {
+//     result += Number(digit);
+//   }
+//   n = result;
+
+// if (result > 9) {
+//     result = 0;
+// }
+// }
+//   return n;
+// }
---
+++ 7kyu.js
@@ -0,0 +1,54 @@
+// ======== Integer Difference  ======== 
+// Write a function that accepts two arguments: an array/list of integers and another integer (n).
+// Determine the number of times where two integers in the array have a difference of n.
+
+// const intDiff = (arr, n) => {
+//   // your code goes here
+//   let result = 0;
+//   for (let i = 1; i < arr.length; i ++){
+//     for (let x = 0; x < i; x ++) {
+//       if (Math.abs(arr[x] - arr[i]) === n){
+//       result ++;
+//      }
+//   }
+//   }
+//   return result;
+// }
+
+
+
+// ======== Find the divisors! ========
+// Create a function named divisors / Divisors that takes an integer n > 1 
+// and returns an array with all of the integer's divisors(except for 1 and the number itself), 
+// from smallest to largest.If the number is prime return the string '(integer) is prime'(null in C#)
+// (use Either String a in Haskell and Result < Vec < u32 >, String > in Rust).
+
+// function divisors(integer) {
+//   let arr = [];
+//   let count = 0;
+//   for (let i = 1; i < 10 || i < integer; i++) {
+//     if (integer % i === 0){
+//       if (i === 1 || i === integer) {
+//         continue;
+//       }
+//       arr.push(i);
+//       count ++;
+//     }
+//   }
+//   if (count === 0) {
+//     return  `${integer} is prime`;
+//   }
+//   return arr;
+// }
+
+// ======== Is this a triangle? ========
+// Implement a method that accepts 3 integer values a, b, c.The method should return true
+// if a triangle can be built with the sides of given length and false in any other case.
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// function isTriangle(a,b,c) {
+//   if (a + b > c && b + c > a && a + c > b){
+//   return true;
+// }
+//    return false;
+// }
