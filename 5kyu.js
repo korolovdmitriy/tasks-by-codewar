5336307bc7de6b613565d8e6c3ce58819f9ec908
Dmitriy Korolov <adrorecords@gmail.com>
2021-09-16 08:28:33 +0300

    Where my anagrams at?

--- 5kyu.js
+++ 5kyu.js
@@ -182,3 +182,52 @@
 
 // Test.assertEquals(validParentheses( "()" ), true);
 // Test.assertEquals(validParentheses( "())" ), false);
+
+
+// ======= Where my anagrams at? =======
+// What is an anagram ? Well, two words are anagrams of each other
+// if they both contain the same letters.For example:
+// 'abba' & 'baab' == true
+// 'abba' & 'bbaa' == true
+// 'abba' & 'abbba' == false
+// 'abba' & 'abca' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+
+// function anagrams(word, words) {
+//     const result = [];
+//     for (const element of words) {
+//         if (element.length === word.length) {
+//             let flag = true;
+//             for (let i = 0; i < word.length; i++) {
+                            
+//                 if (element.match(new RegExp(word[i], "g")) === null) {
+//                     flag = false;
+//                     continue;
+//                 }
+    
+//                 if (word.match(new RegExp(word[i], "g")).length !== element.match(new RegExp(word[i], "g")).length) {
+//                 flag = false;
+//                 };
+                
+//             };
+
+//             if (flag) { result.push(element); };
+                    
+//         };
+//     };
+//     return result;
+// }
+
+
+// console.log(anagrams("abba", ["aabb", "abcd", "bbaa", "dada"]));
+// console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']));
+// console.log(anagrams('laser', ['lazing', 'lazy',  'lacer']));
+
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
