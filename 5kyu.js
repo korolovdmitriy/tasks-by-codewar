b4b725ac098eae96981e71af52545e2af70aad98
Dmitriy Korolov <adrorecords@gmail.com>
2021-09-16 09:53:36 +0300

    Pete, the baker (second solution)

--- 5kyu.js
+++ 5kyu.js
@@ -177,6 +177,22 @@
 // }
 
 
+// ======= SECOND SOLUTION ======
+
+// function cakes(recipe, available) {
+//     let result = [];
+//     for (const key in recipe) {
+//         if (available.hasOwnProperty(key)) {
+//             result.push(Math.floor(available[key] / recipe[key]));
+//             continue;
+//         };
+//         return 0;
+//     };
+
+//     return Math.min(...result);
+// }
+
+
 // console.log(cakes({ flour: 500, sugar: 200, eggs: 1 }, { flour: 1200, sugar: 1200, eggs: 5, milk: 200 }));
 // console.log(cakes({ apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100 }, { sugar: 500, flour: 2000, milk: 2000 }));
 // console.log(cakes({ "apples": 3, "flour": 300, "sugar": 150, "milk": 100, "oil": 100 },
