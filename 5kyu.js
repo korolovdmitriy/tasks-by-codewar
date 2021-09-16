686d368f8507e38730a70aa34e900b263859070c
Dmitriy Korolov <adrorecords@gmail.com>
2021-09-16 15:25:31 +0300

    Regex Password Validation

--- 5kyu.js
+++ 5kyu.js
@@ -358,3 +358,21 @@
 // console.log(scramble('scriptsjava', 'javascripts')); //true)
 // console.log(scramble('jscripts', 'javascript')); //false)
 // console.log(scramble('aabbcamaomsccdd', 'commas')); //true)
+
+
+
+
+// ========= Regex Password Validation ===========
+// You need to write regex that will validate a password to make sure it meets 
+// the following criteria:
+// At least six characters long
+// contains a lowercase letter
+// contains an uppercase letter
+// contains a number
+// Valid passwords will only be alphanumeric characters.
+
+// function validate(password) {
+//   return /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[0-9a-zA-Z]{6,}/g.test(password);
+// }
+
+// console.log(validate('abc'));
