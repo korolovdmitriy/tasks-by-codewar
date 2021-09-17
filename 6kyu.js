90370708bc7f42631b7db90e9c83d7442087351f
Dmitriy Korolov <adrorecords@gmail.com>
2021-09-17 09:35:45 +0300

    Bit Counting

--- 6kyu.js
+++ 6kyu.js
@@ -273,3 +273,20 @@
 // console.log(duplicateCount("aabBcde")); //2
 // console.log(duplicateCount("Indivisibility")); //1
 // console.log(duplicateCount("Indivisibilities")); //2
+
+
+
+//  ======= Bit Counting =======
+// Write a function that takes an integer as input,
+// and returns the number of bits that are equal to one in the binary 
+// representation of that number.You can guarantee that input is non -
+// negative.
+// Example: The binary representation of 1234 is 10011010010,
+// so the function should return 5 in this case
+
+var countBits = function(n) {
+  if (n === 0) {return 0};
+  return n.toString(2).match(new RegExp("1", "gi")).length;
+};
+
+console.log(countBits(10));
