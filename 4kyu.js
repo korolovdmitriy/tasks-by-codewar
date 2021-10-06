9069058ffb883f456dad08bd4ec2dab0264298dd
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-06 09:40:47 +0300

    Most frequently used words in a tex

--- 4kyu.js
+++ 4kyu.js
@@ -87,3 +87,46 @@
 //     console.log(add("888", "222")); // "1110";
 //     console.log(add("1372", "69")); //"1441");
 //     console.log(add("12", "456")); // "468";
+
+
+// ======== Most frequently used words in a text =======
+// Write a function that, given a string of text(possibly with punctuation and line - breaks), 
+// returns an array of the top - 3 most occurring words, in descending order of the number of occurrences.
+// Assumptions:
+// A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII.
+// Apostrophes can appear at the start, middle or end of a word ('abc, abc', 'abc', ab'c are all valid)
+// Any other characters (e.g. #, \, / , . ...) are not part of a word and should be treated as whitespace.
+// Matches should be case-insensitive, and the words in the result should be lowercased.
+// Ties may be broken arbitrarily.
+// If a text contains fewer than three unique words, then either the top - 2 or top - 1 words should be returned,
+// or an empty array if a text contains no words.
+
+// function topThreeWords(text) {
+//         let resultObj = [];
+//     let sortArray = text
+//         .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "")
+//         .toLowerCase()
+//         .split(" ")
+//         .filter(str => str !== "");
+//     if (sortArray[0] === "'") return [];
+
+//     for (let i = 0; i < sortArray.length;) {
+//         resultObj.push({
+//             word: sortArray[0],
+//             count: sortArray.filter((str, i, array) => str === array[0]).length
+//         });
+//         sortArray = sortArray.filter((str, i, array) => str !== array[0]);
+//     };
+
+//     resultObj = resultObj.sort((a, b) => b.count - a.count).slice(0, 3);
+//     const resultArray = resultObj.reduce((total, element) => {
+//         return [...total, element.word]
+//     }, [])
+    
+//     return resultArray;
+// }
+
+// console.log(topThreeWords("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")); //['e','ddd','aa'])
+// console.log(topThreeWords("  //wont won't won't ")); // ["won't", "wont"])
+// console.log(topThreeWords("  , e   .. ")); //["e"])
+// console.log(topThreeWords("'")); //[])
