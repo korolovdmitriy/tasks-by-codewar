a0811f9eea2a0717d52886a267e080829d2059d8
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-06 12:07:54 +0300

    Remove Zeros

--- 4kyu.js
+++ 4kyu.js
@@ -130,3 +130,37 @@
 // console.log(topThreeWords("  //wont won't won't ")); // ["won't", "wont"])
 // console.log(topThreeWords("  , e   .. ")); //["e"])
 // console.log(topThreeWords("'")); //[])
+
+
+//========= Remove Zeros ==========
+// Write a function that takes an array of values and moves all elements that are zero to the end of the array,
+// otherwise preserving the order of the array.The zero elements must also maintain the order in which they occurred.
+// For example, the following array
+// [7, 2, 3, 0, 4, 6, 0, 0, 13, 0, 78, 0, 0, 19, 14]
+// is transformed into
+// [7, 2, 3, 4, 6, 13, 78, 19, 14, 0, 0, 0, 0, 0, 0]
+// Zero elements are defined by either 0 or "0". Some tests may include elements that are not number literals.
+// You are NOT allowed to use any temporary arrays or objects. You are also not allowed to use any 
+// Array.prototype or Object.prototype methods such as .shift(), .push(), etc
+
+// function removeZeros(array) {
+//     let temp;
+//     for (let i = array.length; i >= 0; i--) {
+//         if (array[i] === 0 || array[i] === "0") {
+//             for (let x = i; x < array.length-1; x++) {
+//                 if (array[x + 1] !== 0 && array[x + 1] !== "0") {
+//                     temp = array[x];
+//                     array[x] = array[x + 1];
+//                     array[x + 1] = temp;
+//                 };
+//             };
+//         };
+//     };
+//   return array;
+// }
+
+
+
+// console.log(removeZeros([7, 2, 3, 0, 4, 6, "0", 0, 13, 0, 78, 0, 0, 19, 14]));
+// console.log(removeZeros([ 0, 0, 0, 1, 2, 3 ]));
+// console.log(removeZeros([ 1, null, '5', '0', '2', 0, 8, 6, null, false ]));
