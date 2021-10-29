adaa6251ce21206bcef102105d32992c9e0536df
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-29 12:37:33 +0300

    Unique In Order

--- 6kyu.js
+++ 6kyu.js
@@ -439,3 +439,36 @@
 //                       .join('')
 //   return photoManager.nameExists(res) ? generateName() : res                 
 // }
+
+
+
+//  ========== Unique In Order =========
+// Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to 
+// each other and preserving the original order of elements.
+// For example:
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1,2,2,3,3])       == [1,2,3]
+
+
+// var uniqueInOrder = function (iterable) {
+//     let arrayTemp = [];
+ 
+//     if (Array.isArray(iterable)) {
+//         arrayTemp = iterable;
+//     } else {
+//         arrayTemp = iterable.split('');
+//        }
+        
+//     for (let i = 0; i < arrayTemp.length; i++) {
+//         if (arrayTemp[i] === arrayTemp[i + 1]) {
+//             arrayTemp.splice(i + 1, 1);
+//             i--;        
+//         }
+//     }
+//     return arrayTemp;
+// }
+
+
+// console.log(uniqueInOrder('AAAABBBCCDAABBB')); // ['A','B','C','D','A','B'])
+// console.log(uniqueInOrder([1,2,2,3,3])); // [1,2,3]
