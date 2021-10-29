6de25ee2bdb2b66041c789bf51a6d92dbd9d4ed0
Dmitriy Korolov <adrorecords@gmail.com>
2021-10-29 14:20:22 +0300

    Your order, please

--- 6kyu.js
+++ 6kyu.js
@@ -491,3 +491,24 @@
 // console.log(songDecoder("AWUBBWUBC"));//, "A B C","WUB should be replaced by 1 space");
 // console.log(songDecoder("AWUBWUBWUBBWUBWUBWUBC"));//, "A B C","multiples WUB should be replaced by only 1 space");
 // console.log(songDecoder("WUBAWUBBWUBCWUB"));//, "A B C","heading or trailing spaces should be removed");
+
+
+
+// ======== Your order, please =======
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+// function order(words) {
+//     if (!words) return '';
+//     return words.split(' ').sort((a, b) => parseInt(a.replace(/[^\d]/g, '')) - parseInt(b.replace(/[^\d]/g, ''))).join(' ');
+// }
+
+
+// console.log(order("is2 Thi1s T4est 3a"));//, "Thi1s is2 3a T4est")
+// console.log(order("4of Fo1r pe6ople g3ood th5e the2"));//, "Fo1r the2 g3ood 4of th5e pe6ople")
+// console.log(order(""))//, "", "empty input should return empty string" )
