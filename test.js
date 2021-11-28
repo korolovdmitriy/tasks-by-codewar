306d012d317e222522f4b22024e64e34cd651391
Dmitriy Korolov <adrorecords@gmail.com>
2021-11-28 10:02:52 +0200

    1

--- test.js
+++ test.js
@@ -11,31 +11,54 @@
 // Выход: 3w3b1w.
 
 
-function RunLength(str) {
+// function RunLength(str) {
 
-    let count = 0;
-    let strResult = '';
-	let char = str[0];
+//     let count = 0;
+//     let strResult = '';
+// 	let char = str[0];
 
-	for(let i=0; i < str.length; i++)
-	{
-		if(str[i] == char)
-		{
-			count ++;
-		}
-		else
-		{
-			strResult += count + char;
-			char = str[i];
-			count = 1;
-		}
-	}
+// 	for(let i=0; i < str.length; i++)
+// 	{
+// 		if(str[i] == char)
+// 		{
+// 			count ++;
+// 		}
+// 		else
+// 		{
+// 			strResult += count + char;
+// 			char = str[i];
+// 			count = 1;
+// 		}
+// 	}
 
-    strResult += count + char;
-    str = strResult;
-	return str;
+//     strResult += count + char;
+//     str = strResult;
+// 	return str;
 
-}
+// }
    
-// keep this function call here 
-console.log(RunLength('wwwggopp'));
+// // keep this function call here 
+// console.log(RunLength('wwwggopp'));
+
+
+
+// ========
+
+function getMaxSubSum(arr) {
+  let maxSum = 0; // если элементов не будет - возвращаем 0
+
+  for (let i = 0; i < arr.length; i++) {
+    let sumFixedStart = 0;
+    for (let j = i; j < arr.length; j++) {
+      sumFixedStart += arr[j];
+      maxSum = Math.max(maxSum, sumFixedStart);
+    }
+  }
+  return maxSum;
+}
+
+console.log( getMaxSubSum([-1, 2, 3, -9]) ); // 5
+console.log( getMaxSubSum([-1, 2, 3, -9, 11]) ); // 11
+console.log( getMaxSubSum([-2, -1, 1, 2]) ); // 3
+console.log( getMaxSubSum([1, 2, 3]) ); // 6
+console.log( getMaxSubSum([100, -9, 2, -3, 5]) ); // 100
