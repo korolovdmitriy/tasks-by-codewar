88efee1ac51bb1f5e115eb8eb8225fc0b3617409
Dmitriy Korolov <adrorecords@gmail.com>
2021-12-09 16:42:53 +0200

    update api

--- apiService.js
+++ apiService.js
@@ -1,77 +1,103 @@
 const BASE_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'cfdb2a8aab50d545dc8a1d0938de62d8';
 
-class FilmsApi {
-    constructor(_options) {
-        this.searchQuery = '';
-        this.page = 1;
-        this.totalPages = 500;
-    }
-    async fetchResults() {
-    const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
-    const response = await fetch(urlPopular);
-    const filmsLists = await response.json();
-    return filmsLists;
-    }
-    async fetchSearch() {
-    const urlSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&page=${this.page}&query=${this.searchQuery}`;
-    const response = await fetch(urlSearch);
-    const filmsLists = await response.json();
-    return filmsLists;
-  }
-  async fetchMovies(id) {
-    const urlSearch = `${BASE_URL}movie/${id}?api_key=${API_KEY}`;
-    const response = await fetch(urlSearch);
-    const filmData = await response.json();
-    return filmData;
-  }
-
-  incrementPage() {
-    console.log(this.page);
-    if (this.currentPage === this.totalPages) {
-      return;
-    }
-    this.page += 1;
-  }
-  decrementPage() {
-    if (this.page === 1) {
-      return;
-    }
-    this.page -= 1;
-  }
-  resetPage() {
-    this.page = 1;
-  }
-  get query() {
-    return this.searchQuery;
-  }
-  set query(newQuery) {
-    this.searchQuery = newQuery;
-  }
+async function fetchResults (page) {
+    const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${page}`;
+    const response = await fetch( urlPopular);
+    return await response.json();  
 }
 
-// Получение данных после импорта
-const filmsApi = new FilmsApi(); // создание объекта класса
+// fetchResults(5).then(res => console.log(res.results));
 
-// Получение списка популярных фильмов
-const popularFilms = await filmsApi.fetchResults();
-console.log(popularFilms.results); 
+async function fetchSearch(page, searchQuery) {
+    const urlSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&page=${page}&query=${searchQuery}`;
+    const response = await fetch (urlSearch);
+    return await response.json();    
+}
+  
+// fetchSearch(1, 'Dune').then(res => console.log(res.results));
 
-// Получение списка фильмов поиску
-filmsApi.query = 'Dune'; // запись в клас поискового запроса
-const filmInfo = await filmsApi.fetchSearch();
-console.log(filmInfo.results);
+async function fetchMovies(id) {
+    const urlSearch = `${BASE_URL}movie/${id}?api_key=${API_KEY}`;
+    const response = await fetch (urlSearch);
+    return await response.json();
+}
+  
+// fetchMovies(580489).then(res => console.log(res));
 
-// Получение фильма по ID
-const id = 580489;
-const filmById = await filmsApi.fetchMovies(id);
-console.log(filmById);
+export { fetchResults, fetchSearch, fetchMovies };
 
-// прибавление номера страницы для пагинации
-filmsApi.incrementPage();
+// class FilmsApi {
+//     constructor() {
+//         this.searchQuery = '';
+//         this.page = 1;
+//         this.totalPages = 500;
+//     }
+//     async fetchResults() {
+//     const urlPopular = `${BASE_URL}trending/movie/day?api_key=${API_KEY}&page=${this.page}`;
+//     const response = await fetch(urlPopular);
+//     const filmsLists = await response.json();
+//     return filmsLists;
+//     }
+//     async fetchSearch() {
+//     const urlSearch = `${BASE_URL}search/movie?api_key=${API_KEY}&page=${this.page}&query=${this.searchQuery}`;
+//     const response = await fetch(urlSearch);
+//     const filmsLists = await response.json();
+//     return filmsLists;
+//   }
+//   async fetchMovies(id) {
+//     const urlSearch = `${BASE_URL}movie/${id}?api_key=${API_KEY}`;
+//     const response = await fetch(urlSearch);
+//     const filmData = await response.json();
+//     return filmData;
+//   }
 
-// уменьшение номера страницы для пагинации
-filmsApi.decrementPage();
+//   incrementPage() {
+//     console.log(this.page);
+//     if (this.currentPage === this.totalPages) {
+//       return;
+//     }
+//     this.page += 1;
+//   }
+//   decrementPage() {
+//     if (this.page === 1) {
+//       return;
+//     }
+//     this.page -= 1;
+//   }
+//   resetPage() {
+//     this.page = 1;
+//   }
+//   get query() {
+//     return this.searchQuery;
+//   }
+//   set query(newQuery) {
+//     this.searchQuery = newQuery;
+//   }
+// }
 
-// обнуление страницы 
-filmsApi.resetPage();
+// // Получение данных после импорта
+// const filmsApi = new FilmsApi(); // создание объекта класса
+
+// // Получение списка популярных фильмов
+// const popularFilms = await filmsApi.fetchResults();
+// console.log(popularFilms.results); 
+
+// // Получение списка фильмов поиску
+// filmsApi.query = 'Dune'; // запись в клас поискового запроса
+// const filmInfo = await filmsApi.fetchSearch();
+// console.log(filmInfo.results);
+
+// // Получение фильма по ID
+// const id = 580489;
+// const filmById = await filmsApi.fetchMovies(id);
+// console.log(filmById);
+
+// // прибавление номера страницы для пагинации
+// filmsApi.incrementPage();
+
+// // уменьшение номера страницы для пагинации
+// filmsApi.decrementPage();
+
+// // обнуление страницы 
+// filmsApi.resetPage();
